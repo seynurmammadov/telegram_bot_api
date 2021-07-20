b49af9e23f2452a6879a2f6fae58b31aff1fb720
seynurmammadov <seynursm@code.edu.az>
2021-07-21 00:02:47 +0400

    init

---
+++ src/main/java/az/code/telegram_bot_api/TelegramBotApiApplication.java
@@ -0,0 +1,13 @@
+package az.code.telegram_bot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApiApplication.class, args);
+    }
+
+}
