745ba760e02ca7c32b21828af082b8c47cac4582
seynurmammadov <seynursm@code.edu.az>
2021-07-29 01:19:01 +0400

    added TokenUtil testing

--- src/main/java/az/code/telegram_bot_api/schedules/OfferCleaningSchedule.java
+++ src/main/java/az/code/telegram_bot_api/schedules/OfferCleaningSchedule.java
@@ -1,12 +1,9 @@
 package az.code.telegram_bot_api.schedules;
 
-import az.code.telegram_bot_api.models.UserRequest;
 import az.code.telegram_bot_api.repositories.UserRequestRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 @Slf4j
--- src/main/java/az/code/telegram_bot_api/utils/TokenUtil.java
+++ src/main/java/az/code/telegram_bot_api/utils/TokenUtil.java
@@ -1,5 +1,6 @@
 package az.code.telegram_bot_api.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import java.util.Base64;
 
 @Component
 public class TokenUtil {
-    public JsonNode getUser(String auth) throws com.fasterxml.jackson.core.JsonProcessingException {
+    public JsonNode getUser(String auth) throws JsonProcessingException {
         String[] chunks = auth.split("\\.");
         String data = new String(Base64.getDecoder().decode(chunks[1]));
         return new ObjectMapper().readValue(data, JsonNode.class);
